1cc1f997fa904fd9303c9bad8de72308b4e40e38
adofsauron <adofsauron@163.com>
2016-01-29 15:57:16 +0800

    fix style

--- 3Sum Closest/3Sum Closest.cpp
+++ 3Sum Closest/3Sum Closest.cpp
@@ -6,21 +6,33 @@
 */
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
+    int threeSumClosest(vector<int>& nums, int target) 
+    {
         sort(nums.begin(),nums.end());
         int res = nums[0] + nums[1] + nums[2];
+
         int n = nums.size();
-        for(int i = 0; i < n && res != target; i++){            //枚举a
-            if(i && nums[i] == nums[i-1])     continue;         //排除重复的解
+        for(int i = 0; i < n && res != target; i++)
+        {            //枚举a
+            if(i && nums[i] == nums[i-1])     
+            	continue;         								//排除重复的解
+            
             int lhs = i+1,rhs = n-1;                            //b和c的下标
-            while(rhs > lhs){                                   //O(n)枚举b和c
+            while(rhs > lhs)
+            {                                   				//O(n)枚举b和c
                  int tmp = nums[i] + nums[lhs] + nums[rhs];
-                 if(abs(tmp - target) < abs(res - target)){     
+
+                 if(abs(tmp - target) < abs(res - target))
+                 {     
                      res = tmp;                                 //当前解比原来解好  替换
                  }
-                 if(tmp > target)   rhs--;
-                 else if(tmp < target)   lhs++;
-                 else   break;
+
+                 if(tmp > target)  
+                 	rhs--;
+                 else if(tmp < target)   
+                 	lhs++;
+                 else   
+                 	break;
             }
         }
         return res;
--- Palindrome Number/Palindrome Number.cpp
+++ Palindrome Number/Palindrome Number.cpp
@@ -1,22 +1,29 @@
 //解法很直接，把x的每一位分离出来，然后进行前后比较就可以了
 
-class Solution {
+class Solution
+ {
 public:
-    bool isPalindrome(int x) {
+    bool isPalindrome(int x)
+    {
         if (x < 0)  return false;
         if (x < 10)  return true;
         int tmp = x;
         int len = 0;
-        while(tmp){
+        while(tmp)
+        {
             tmp /= 10, ++len;
         }
+
         int left = pow(10, len - 1);
         int right = 1;
-        while(left > right){
+
+        while(left > right)
+        {
             if (x / left % 10 != x / right % 10)  return false;
             left /= 10;
             right *= 10;
         }
+
         return true;
     }
 };
